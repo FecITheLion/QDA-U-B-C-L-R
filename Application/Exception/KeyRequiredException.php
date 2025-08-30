ec4c873457c6abc5d975570765e23156c08b591a
FecITheLion <cieeiye@gmail.com>
2025-08-30 00:39:31 -0700

    Create KeyRequiredException.php

---
+++ Application/Exception/KeyRequiredException.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Thrown when an operation cannot be completed because a value is not unique
+ * and requires a specific key to be identified.
+ */
+class KeyRequiredException extends \LogicException {}
